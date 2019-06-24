74b8c3a2d1481713832a735f7a967bbc320a6466
marc <marc.inthaamnouay@gmail.com>
2019-06-24 22:33:19 +0200

    wip parsing arrays

--- src/docker/yaml/compose.rs
+++ src/docker/yaml/compose.rs
@@ -5,6 +5,7 @@
  */
 pub mod compose {
   use yaml_rust::{yaml};
+  use std::collections::HashMap;
 
   // constant error
   const EMPTY_YAML_CONTENT: &str = "Unable to parse empty content of docker-compose.yaml file";
@@ -31,21 +32,36 @@ pub mod compose {
     service: Vec<Service>
   }
 
+  enum FieldKind {
+    SingleField,
+    ArrayField
+  }
+
   /**
    * Get Supported attributes
    * 
    * Return a vector of the supported list of attributes
    */
-  fn get_supported_attributes() -> Vec<&'static str> {
-    let vec: Vec<&'static str> = vec![
-      "name",
-      "image",
-      "port",
-      "labels",
-      "replicas"
-    ];
-
-    return vec;
+  fn get_supported_attributes(field: FieldKind) -> Vec<&'static str> {
+    match field {
+      FieldKind::SingleField => {
+        return vec![
+          "name",
+          "image",
+          "labels",
+          "replicas"
+        ];
+      },
+      FieldKind::ArrayField => {
+        return vec![
+          "ports",
+          "command",
+          "environment",
+          "volumes",
+          "labels"
+        ];
+      }
+    };
   }
 
   /**
@@ -83,16 +99,32 @@ pub mod compose {
    * Get attribute value for each services
    */
   fn get_service(yaml_service: yaml::Yaml) -> Service {
-    let vec: Vec<&str> = get_supported_attributes()
+    let str_field_vec: Vec<&str> = get_supported_attributes(FieldKind::SingleField)
         .into_iter()
         .map(|key| yaml_service[key].as_str().unwrap_or(""))
         .collect();
 
+    let mut array_attributes = HashMap::new();
+    let attributes = get_supported_attributes(FieldKind::ArrayField);
+
+    for attr in attributes.into_iter() {
+      let vec = yaml_service[attr].as_vec();
+      if let Some(array) = vec {
+        let str_vec_fields: Vec<&str> = array
+          .into_iter()
+          .map(|value| value.as_str().unwrap_or(""))
+          .collect();
+        
+        array_attributes.insert(attr, str_vec_fields);        
+      }
+    }
+
+    println!("value of str_field {:?}", array_attributes);
 
     Service {
-      name: String::from(vec[0]),
-      image: String::from(vec[1]),
-      port: String::from(vec[2]),
+      name: String::from(str_field_vec[0]),
+      image: String::from(str_field_vec[1]),
+      port: String::from(str_field_vec[2]),
       ..Default::default()
     }
   }
