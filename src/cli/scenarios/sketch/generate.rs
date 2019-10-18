2e6d4033bd49378f4ab0c7508d2f3d590489a290
marc <marc.inthaamnouay@gmail.com>
2019-10-18 23:33:35 +0200

    wip

--- src/cli/scenarios/sketch/generate.rs
+++ src/cli/scenarios/sketch/generate.rs
@@ -2,6 +2,8 @@ use crate::docker::{lexer::lexer, parser};
 use crate::cli::core::logger::logger::{log, LogType};
 use crate::kubernetes::{tree, io};
 use crate::confiture::config::conf;
+use crate::cli::scenarios::sketch::helper;
+use crate::cli::scenarios::scenes::picker::EnumHelper;
 use crate::errors::cli_error::{CliErr, ErrHelper, ErrCode};
 
 /// Constant referring to the compose file which need to be parse
@@ -12,6 +14,23 @@ const PREPARE_PARSING: &str = "Preparing to parse the docker-compose.yml located
 const ERROR_GET_DOCKER_SERVICE_LIST: &str = "Unable to retrieve list of services in the docker-compose";
 const ERROR_GET_CONFITURE: &str = "Unable to parse the confiture.json file as it's empty";
 
+/// Generate Options
+/// 
+/// # Description
+/// Supported command options
+enum GenerateOptions {
+    Print
+}
+
+impl EnumHelper<GenerateOptions> for GenerateOptions {
+    fn from_string(action: &String) -> Option<GenerateOptions> {
+        match action.to_lowercase().as_str() {
+            "print" => Some(GenerateOptions::Print),
+            _ => None
+        }
+    }
+}
+
 /// Launch
 /// 
 /// # Description
@@ -21,7 +40,7 @@ const ERROR_GET_CONFITURE: &str = "Unable to parse the confiture.json file as it
 /// 
 /// # Arguments
 /// * `sub_action`: slice of string representing the path
-pub fn launch(sub_action: &str) {
+pub fn launch(sub_action: &str, options: &Vec<String>) {
     log(
         LogType::Info,
         PREPARE_PARSING,
@@ -45,13 +64,14 @@ pub fn launch(sub_action: &str) {
                 ErrCode::ParsingError
             ).log_pretty();
 
-        return;
+            return;
         }
     };
 
     let confiture_opts = conf::load_conf(String::new(), sub_action);
     if let Some(conf) = confiture_opts {
         let kubes = tree::tree::get_kube_abstract_tree(services, conf);
+        let opts = retrieve_cmd_options(options);
         match io::bootstrap::bootstrap::prepare_kube(&kubes) {
             Ok(()) => io::writer::writer::write_kubernetes_yaml(kubes),
             Err(e) => e.log_pretty()
@@ -65,4 +85,15 @@ pub fn launch(sub_action: &str) {
         String::new(),
         ErrCode::MissingFieldError
     ).log_pretty();
+}
+
+/// Retrieve Cmd Options
+/// 
+fn retrieve_cmd_options(options: &Vec<String>) -> Option<GenerateOptions> {
+    let opt = match helper::retrieve_options_by_idx(options, 0) {
+        Some(p) => p,
+        None => String::new()
+    };
+
+    GenerateOptions::from_string(&opt)
 }
