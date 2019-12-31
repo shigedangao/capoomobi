93ec7dd4de729935413f0cc65e4bc0a5d65c4100
marc <marc.inthaamnouay@gmail.com>
2019-12-31 16:07:54 +0100

    test: add other unit test

--- src/tests/docker.rs
+++ src/tests/docker.rs
@@ -33,8 +33,53 @@ mod parser {
         };
 
         match parser::get_docker_services(yaml) {
+            Some(_) => assert!(true),
+            None => panic!("No services has been founded")
+        };
+    }
+
+    #[test]
+    fn test_parse_empty_docker_svc() {
+        match parser::get_docker_services(vec![]) {
+            Some(_) => panic!("Expect to have return None"),
+            None => assert!(true)
+        }
+    }
+
+    #[test]
+    fn test_retrieve_proper_number_services() {
+        let yaml = match loader::load("./example", "docker-compose.yaml") {
+            Ok(res) => res,
+            Err(err) => panic!(err)
+        };
+
+        let contents = match parser::get_docker_services(yaml) {
             Some(res) => res,
             None => panic!("No services has been founded")
         };
+
+        if contents.len() != 3 {
+            panic!("Expect to have retrieve 3 services");
+        }
+    }
+
+    #[test]
+    fn test_retrieve_proper_values() {
+        let yaml = match loader::load("./example", "docker-compose.yaml") {
+            Ok(res) => res,
+            Err(err) => panic!(err)
+        };
+
+        let contents = match parser::get_docker_services(yaml) {
+            Some(res) => res,
+            None => panic!("No services has been founded")
+        };
+
+        assert_eq!(contents[0].image, "golang:1.12.6-stretch");
+        assert_eq!(contents[0].name, "web");
+        assert_eq!(contents[0].commands[0], "sh start.sh");
+        assert_eq!(contents[0].labels, vec!["go", "api"]);
+        assert_eq!(contents[0].environment, vec!["GO111MODULE=on"]);
+        assert_eq!(contents[0].ports, vec!["5000:5000"]);
     }
 }
